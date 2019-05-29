c56f778df79c74cd92d3719573213453ea7cafec
ALIENONE\alexg <alex.gamboa@minecloudapps.com>
2019-05-29 15:24:48 -0600

    alexGamboa-add db schemas

--- modules/db/db.js
+++ modules/db/db.js
@@ -15,14 +15,43 @@ const ticketSchema =
         }
     })
 
-// const userSchema;
-// const assetSchema;
+const assetCategorySchema =
+    new mongoose.Schema({
+        name: String
+    })
 
-const TicketModel = mongoose.model('Ticket',ticketSchema)
+const assetSchema =
+    new mongoose.Schema({
+        dateOfCreation: { type: Date, default: Date.now},
+        category: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'AssetCategory'
+        },
+        isWithoutControlNumber: Boolean
+    })
+
+// const userSchema;
+
+const TicketModel = mongoose.model('Ticket', ticketSchema)
+const AssetCateogyModel = mongoose.model('AssetCategory', assetCategorySchema)
+const AssetModel = mongoose.model('Asset', assetSchema)
 
 function getTicketInstance(obj){
         return new TicketModel(obj)
 }
 
+function getAssetCategoryInstance(obj) {
+    return new AssetCateogyModel(obj)
+}
+
+function getAssetInstance(obj) {
+    return new AssetModel(obj)
+}
+
 module.exports.getTicketInstance = getTicketInstance
+module.exports.getAssetCategoryInstance = getAssetCategoryInstance
+module.exports.getAssetInstance = getAssetInstance
+
 module.exports.TicketModel = TicketModel
+module.exports.AssetCateogyModel = AssetCateogyModel
+module.exports.AssetModel = AssetModel
